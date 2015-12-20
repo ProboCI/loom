c936d3f69bbede7b19369f8cb7bcd0b0f21db837
Ilya Braude <ilya@bluenexa.com>
2015-12-20 18:22:09 -0500

    upgraded config to use yaml config instead of env variables/files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var server = require('./lib/api/server');
-var config = require('./lib/config');
+module.exports.run = function run(config){
+  var server = require('./lib/api/server');
 
-server.log.info({config: config}, "Configuration loaded");
+  server.log.info({config: config}, "Configuration loaded");
 
-// connect to the DB
-var rethink = require('./lib/rethink')
-rethink.connect(config.db)
+  // connect to the DB
+  var rethink = require('./lib/rethink')
+  rethink.connect(config.db)
 
-server.listen(config.server.port, config.server.host, function() {
-  server.log.info('%s listening at %s', server.name, server.url);
-});
+  server.listen(config.server.port, config.server.host, function() {
+    server.log.info('%s listening at %s', server.name, server.url);
+  });
+}
